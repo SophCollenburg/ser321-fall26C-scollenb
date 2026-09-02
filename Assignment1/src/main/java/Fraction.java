cf44b293f92831be4a45bce04fa22cc11515fdbb
SophCollenburg <163323775+SophCollenburg@users.noreply.github.com>
2026-09-02 16:34:18 -0500

    Fix error

--- Assignment1/src/main/java/Fraction.java
+++ Assignment1/src/main/java/Fraction.java
@@ -42,7 +42,7 @@ public class Fraction {
          // create a new instance
          // Fraction *frac = [[Fraction alloc] init];
          if(args.length != 2){
-            System.out.printf("Error: Not enough args %d\n," args.length);
+            System.out.println("Error: Incorrect args");
             System.exit(0);
 
             int num = Integer.parseInt(args[0]);
